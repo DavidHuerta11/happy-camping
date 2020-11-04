5aedae11206a49d683737a5072aac8c8218a7f80
David Huerta <63136889+DavidHuerta11@users.noreply.github.com>
2020-11-03 23:11:28 -0600

    Update create.sql

--- server/create.sql
+++ server/create.sql
@@ -1,28 +1,17 @@
 -- CREATE DATABASE happycamping;
 -- RUN THIS SCRIPT FIRST
 
-CREATE TABLE locations(
-    location_id INTEGER PRIMARY KEY,
-    city VARCHAR(100),
-    state_abbreviation VARCHAR(100),
-    state_name VARCHAR(100),
-    country_abbreviation VARCHAR(100),
+CREATE TABLE countries(
+    country_id SERIAL PRIMARY KEY,
+    country_code VARCHAR(100),
     country_name VARCHAR(100)
 );
 
-CREATE OR REPLACE
-  FUNCTION replace_json_keys(IN js TEXT) 
-  RETURNS json 
-  STABLE 
-  AS
-$$
-BEGIN
-  js := replace(js, '"id"', '"location_id"');
-  js := replace(js, '"name"', '"city"');
-  js := replace(js, '"state"', '"state_abbreviation"');
-  js := replace(js, '"country"', '"country_abbreviation"');
-  return js::json;
-END;
-$$ LANGUAGE 'plpgsql';
+CREATE TABLE states(
+    state_id SERIAL PRIMARY KEY,
+    country_id INT NOT NULL REFERENCES countries(country_id),
+    state_code VARCHAR(100),
+    state_name VARCHAR(100)
+);
 
 
